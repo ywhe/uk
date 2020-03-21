ddd3c104d02136757450a9ce8fbfbbcf8c18fba6
Yanwei.He <ywhe@outlook.com>
2020-03-21 21:11:38 +0000

    update data source

--- src/App.js
+++ src/App.js
@@ -4,61 +4,64 @@ import Summary from './components/Summary'
 import SimpleChart from './components/SimpleChart'
 import ConfirmedMap from './components/ConfirmedMap'
 
-function App() {
-  let summaryData={
-    Date:'1995-04-07T00:00:00',
-    NewConfirmedCount:10,
-    ConfirmedCount:1000,
-    TestedCount:20000,
-    NewTestedCount:2000,
-    DeathsCount:144,
-    NewDeathsCount:30,
-    LondonCount:800,
-    NewLondonCount:0
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      labels:['aaaa','bbbbbbb','cccccccc'],
+      confirmedData:[1,22,32],
+      summary:{},
+      details:[]
+    };
   }
-  let labels=['aaaa','bbbbbbb','cccccccc'];
-  let cdata=[1,30,15];
-  let confirmedData=[1,22,32];
-
-  return (
-    <div className="App">
-      <div className="row">
-        <div className="col">
-          <Summary data={summaryData}></Summary>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-sm" style={{minHeight:'300px'}}>
-          <SimpleChart type="line" title="Cumulative Confirmed" color="red" labels={labels} data={confirmedData}></SimpleChart>
-        </div>
-        <div className="col-sm" style={{minHeight:'300px'}}>
-          <SimpleChart type="line" title="Cumulative Deaths" color="black" labels={labels} data={confirmedData}></SimpleChart>
-        </div>
-        <div className="col-sm" style={{minHeight:'300px'}}>
-          <SimpleChart type="line" title="Cumulative Tested" color="green" labels={labels} data={confirmedData}></SimpleChart>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-sm" style={{minHeight:'300px'}}>
-          <SimpleChart type="bar" title="Daily Confirmed" color="red" labels={labels} data={confirmedData}></SimpleChart>
-        </div>
-        <div className="col-sm" style={{minHeight:'300px'}}>
-          <SimpleChart type="bar" title="Daily Deaths" color="black" labels={labels} data={confirmedData}></SimpleChart>
-        </div>
-        <div className="col-sm" style={{minHeight:'300px'}}>
-          <SimpleChart type="bar" title="Daily Tested" color="green" labels={labels} data={confirmedData}></SimpleChart>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-sm-8" style={{height:'100vh'}}>
-            <ConfirmedMap records={[]}></ConfirmedMap>
+  componentDidMount(){
+    let date=new Date();
+    let url="https://raw.githubusercontent.com/ywhe/UK-COVID-19-Daily-Update/master/data.json"
+    fetch(url)
+    .then(response=>response.json())
+    .then(data=>this.setState(data));
+  }
+  render(){
+    return (
+      <div className="App">
+        <div className="row">
+          <div className="col">
+            <Summary data={this.state.summary}></Summary>
           </div>
-        <div className='col-sm'>
-            table here 
+        </div>
+        <div className="row">
+          <div className="col-sm" style={{minHeight:'300px'}}>
+            <SimpleChart type="line" title="Cumulative Confirmed" color="red" labels={this.state.labels} data={this.state.confirmedData}></SimpleChart>
+          </div>
+          <div className="col-sm" style={{minHeight:'300px'}}>
+            <SimpleChart type="line" title="Cumulative Deaths" color="black" labels={this.state.labels} data={this.state.confirmedData}></SimpleChart>
+          </div>
+          <div className="col-sm" style={{minHeight:'300px'}}>
+            <SimpleChart type="line" title="Cumulative Tested" color="green" labels={this.state.labels} data={this.state.confirmedData}></SimpleChart>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm" style={{minHeight:'300px'}}>
+            <SimpleChart type="bar" title="Daily Confirmed" color="red" labels={this.state.labels} data={this.state.confirmedData}></SimpleChart>
+          </div>
+          <div className="col-sm" style={{minHeight:'300px'}}>
+            <SimpleChart type="bar" title="Daily Deaths" color="black" labels={this.state.labels} data={this.state.confirmedData}></SimpleChart>
+          </div>
+          <div className="col-sm" style={{minHeight:'300px'}}>
+            <SimpleChart type="bar" title="Daily Tested" color="green" labels={this.state.labels} data={this.state.confirmedData}></SimpleChart>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-8" style={{height:'100vh'}}>
+              <ConfirmedMap records={this.state.details}></ConfirmedMap>
+            </div>
+          <div className='col-sm'>
+              table here 
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } 
 }
 
 export default App;
