302f6ce78849c6b72103367f287cbb5a9ebf57fa
Yanwei.He <ywhe@outlook.com>
2020-03-21 22:21:42 +0000

    add data file

--- src/App.js
+++ src/App.js
@@ -8,52 +8,65 @@ class App extends React.Component {
   constructor(props){
     super(props);
     this.state={
-      labels:['aaaa','bbbbbbb','cccccccc'],
-      confirmedData:[1,22,32],
-      summary:{},
-      details:[]
+      data:{labels:[],
+        confirmedData:[],
+        summary:{},
+        details:[]},
+      isLoading: true,
     };
   }
   componentDidMount(){
-    let date=new Date();
-    let url="https://raw.githubusercontent.com/ywhe/UK-COVID-19-Daily-Update/master/data.json"
+    this.setState({isLoading:true});
+    let url="https://raw.githubusercontent.com/ywhe/uk/master/src/data.json"
     fetch(url)
-    .then(response=>response.json())
-    .then(data=>this.setState(data));
+    .then(response=>{
+      console.log(response);
+      return response.json()})
+    .then(data=>{
+      this.setState({
+        data:data,
+        isLoading:false
+      });
+      console.log(this.state);
+    });
   }
   render(){
+    if(this.state.isLoading)
+    {
+        return (<div>is loading.....</div>);
+    }
     return (
       <div className="App">
         <div className="row">
           <div className="col">
-            <Summary data={this.state.summary}></Summary>
+            <Summary data={this.state.data.summary}></Summary>
           </div>
         </div>
         <div className="row">
           <div className="col-sm" style={{minHeight:'300px'}}>
-            <SimpleChart type="line" title="Cumulative Confirmed" color="red" labels={this.state.labels} data={this.state.confirmedData}></SimpleChart>
+            <SimpleChart type="line" title="Cumulative Confirmed" color="red" labels={this.state.data.labels} data={this.state.data.confirmedData}></SimpleChart>
           </div>
           <div className="col-sm" style={{minHeight:'300px'}}>
-            <SimpleChart type="line" title="Cumulative Deaths" color="black" labels={this.state.labels} data={this.state.confirmedData}></SimpleChart>
+            <SimpleChart type="line" title="Cumulative Deaths" color="black" labels={this.state.data.labels} data={this.state.data.confirmedData}></SimpleChart>
           </div>
           <div className="col-sm" style={{minHeight:'300px'}}>
-            <SimpleChart type="line" title="Cumulative Tested" color="green" labels={this.state.labels} data={this.state.confirmedData}></SimpleChart>
+            <SimpleChart type="line" title="Cumulative Tested" color="green" labels={this.state.data.labels} data={this.state.data.confirmedData}></SimpleChart>
           </div>
         </div>
         <div className="row">
           <div className="col-sm" style={{minHeight:'300px'}}>
-            <SimpleChart type="bar" title="Daily Confirmed" color="red" labels={this.state.labels} data={this.state.confirmedData}></SimpleChart>
+            <SimpleChart type="bar" title="Daily Confirmed" color="red" labels={this.state.data.labels} data={this.state.data.confirmedData}></SimpleChart>
           </div>
           <div className="col-sm" style={{minHeight:'300px'}}>
-            <SimpleChart type="bar" title="Daily Deaths" color="black" labels={this.state.labels} data={this.state.confirmedData}></SimpleChart>
+            <SimpleChart type="bar" title="Daily Deaths" color="black" labels={this.state.data.labels} data={this.state.data.confirmedData}></SimpleChart>
           </div>
           <div className="col-sm" style={{minHeight:'300px'}}>
-            <SimpleChart type="bar" title="Daily Tested" color="green" labels={this.state.labels} data={this.state.confirmedData}></SimpleChart>
+            <SimpleChart type="bar" title="Daily Tested" color="green" labels={this.state.data.labels} data={this.state.data.confirmedData}></SimpleChart>
           </div>
         </div>
         <div className="row">
           <div className="col-sm-8" style={{height:'100vh'}}>
-              <ConfirmedMap records={this.state.details}></ConfirmedMap>
+              <ConfirmedMap records={this.state.data.details}></ConfirmedMap>
             </div>
           <div className='col-sm'>
               table here 
